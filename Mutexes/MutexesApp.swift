6e88be8e3c12fc3ba22e460873d5a91b32ab7283
Romain Rabouan <romain.rabouan@withings.com>
2024-09-02 21:33:44 +0200

    Initial Commit

---
+++ Mutexes/MutexesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutexesApp.swift
+//  Mutexes
+//
+//  Created by Romain Rabouan on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MutexesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
